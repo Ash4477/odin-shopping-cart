55ed58d99a7753e8e74c7929f30775468ad9b164
Adil <115704572+Ash4477@users.noreply.github.com>
2025-04-28 18:23:34 +0500

    built header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,37 @@
+import CONSTANTS from "../data/constants";
+import styled from "styled-components";
+import Navbar from "./Navbar";
+import { FlexDiv, ImageHolder, Image, Button } from "./UIComponents";
+
+const Container = styled.header`
+  border: 1px solid red;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 3rem;
+`;
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <FlexDiv>
+        <ImageHolder>
+          <Image src={CONSTANTS.logoImage} />
+        </ImageHolder>
+        <h2 style={{ fontSize: "1.8rem" }}>SneakShop</h2>
+      </FlexDiv>
+      <Navbar />
+      <FlexDiv $gap="1.5rem">
+        <ImageHolder>
+          <Image src={CONSTANTS.cartImage} />
+        </ImageHolder>
+        <ImageHolder>
+          <Image src={CONSTANTS.magnifyImage} />
+        </ImageHolder>
+        <Button>Log In</Button>
+      </FlexDiv>
+    </Container>
+  );
 };
 
 export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,85 @@
+import { Link, useLocation } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+import CONSTANTS from "../data/constants";
+
+const slideLeftToRight = keyframes`
+  from {
+    width: 0%;
+  }
+
+  to {
+    width: 100%;
+  }
+`;
+
+const slideRightToLeft = keyframes`
+  from {
+    width: 100%;
+  }
+  to {
+    width: 0%;
+  }
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  list-style-type: none;
+  gap: 1.5rem;
+`;
+
+const StyledLink = styled(Link)`
+  font-weight: ${(props) => (props.$active ? "bold" : "normal")};
+  font-size: 1.2rem;
+  text-decoration: none;
+  color: inherit;
+  position: relative;
+  padding-bottom: 2px;
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0%;
+    height: 2px;
+    background-color: ${CONSTANTS.primaryColor};
+    transition: none;
+  }
+
+  &:hover::after {
+    animation: ${slideLeftToRight} 0.2s ease-in-out forwards;
+  }
+
+  &:not(:hover)::after {
+    animation: ${slideRightToLeft} 0.2s ease-in-out forwards;
+  }
+`;
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const location = useLocation();
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledLink to="/" $active={location.pathname === "/"}>
+            Home
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/shop" $active={location.pathname === "/shop"}>
+            Shop
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink
+            to="/contacts"
+            $active={location.pathname === "/contacts"}
+          >
+            Contacts
+          </StyledLink>
+        </li>
+      </NavList>
+    </nav>
+  );
 };
 
 export default Navbar;
