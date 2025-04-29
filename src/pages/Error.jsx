02a7e14a789c19e11580fcd7cd8fda12c447b583
Adil <115704572+Ash4477@users.noreply.github.com>
2025-04-29 21:26:52 +0500

    add custom error pag

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,35 @@
+import styled from "styled-components";
+import CONSTANTS from "../data/constants";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  min-height: 100vh;
+  background-color: ${CONSTANTS.tertiaryColor};
+  color: ${CONSTANTS.primaryColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 5px solid ${CONSTANTS.secodaryColor};
+  padding: 5rem;
+`;
+
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Container>
+      <Div>
+        <h1>Error: 404</h1>
+        <h2>This route doesn't exist! :&lt;</h2>
+        <Link to="/">Click here to go to home</Link>
+      </Div>
+    </Container>
+  );
 };
 
 export default Error;
